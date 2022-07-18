e6fbf3708a36add1dbff04a38eac7a214eb02144
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-18 13:14:57 +0900

    바닐자 자바스킬븥 화이팅

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@
 
 let form=document.querySelector("#todo-form");
 let bt=document.querySelector(".submit");
+let head=document.querySelector("h1")
+let list=document.querySelector("#todo-list");
 //선택자
 bt.onclick=function (){
-    alert("클릭했습니다")
+    form.style.backgroundColor=`red`;
+    head.textContent="Hello";
 }
+//색깔 바꾸기
+form.style.backgroundColor=`blue`;
+//텍스트 바꾸기
+// head.textContent="Hello";
+//태그 변경하기
+list.lastElementChild.innerHTML="<h1>HelloWorld</h1>";
+//태그 추가하기
