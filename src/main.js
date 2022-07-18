1045e36b3ca7607632f69a9da5696b37c91ac93e
주동호 <100278301+JOODON@users.noreply.github.com>
2022-07-18 11:22:01 +0900

    선택자와 버튼 클릭!

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// let form=document.getElementById(`todo-form`);
+
+let form=document.querySelector("#todo-form");
+let bt=document.querySelector(".submit");
+//선택자
+bt.onclick=function (){
+    alert("클릭했습니다")
+}
